9f88ea92a051ced7345e76ad841d7ee94b6bf5e5
PhiSpel <philipp.spelten@web.de>
2024-11-29 14:40:03 +0100

    diag-suppress only for cuda

--- default.mk
+++ default.mk
@@ -6,11 +6,11 @@ export MPI_ROOT := ${MPI_ROOT}#/home/hbrs/openmpi#/usr/lib/x86_64-linux-gnu/open
 
 # Include config.mk environment (optional)
 -include $(OLB_ROOT)/config.mk
-CXXFLAGS		+= -diag-suppress 20012
 # LIBS = -L/lib/x86_64-linux-gnu/ -lhwloc  # libhwloc.so.15:$(LD_LIBRARY_PATH)
 
 # Select mixed compilation mode if separate CUDA compiler is given
 ifdef CUDA_CXX
+CXXFLAGS		+= -diag-suppress 20012
 include $(OLB_ROOT)/default.mixed.mk
 # otherwise use single CXX for all of OpenLB
 else
