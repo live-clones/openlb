ac4bbb2df8ab430857451a69d001e87265e151a0
PhiSpel <philipp.spelten@web.de>
2025-02-21 15:26:02 +0100

    another intermediary update

--- examples/gridRefinement/du93/du93.cpp
+++ examples/gridRefinement/du93/du93.cpp
@@ -342,6 +342,7 @@ int main(int argc, char* argv[])
   // IndicatorLayer3D<T> toBeRefinedLayerI( toBeRefinedI, dx1 );
   cGeometryL1.remove( toBeRefinedI );
   cGeometryL1.refine(2);
+  cGeometryL1.setPeriodicity( {false, false, false} );
   cGeometryL1.print();
 
   auto cGeometryL2 = cGeometryL1;
@@ -349,6 +350,7 @@ int main(int argc, char* argv[])
   // IndicatorLayer3D<T> toBeRefinedLayerII( toBeRefinedII, dx2 );
   cGeometryL2.remove( toBeRefinedII );
   cGeometryL2.refine(2);
+  cGeometryL2.setPeriodicity( {false, false, false} );
   cGeometryL2.print();
 
   //// ADJUST WEIGHTS FOR BALANCING ////
