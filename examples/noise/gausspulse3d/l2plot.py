16ee3804fac6125f3af4b3610b2af1820b6f0e6c
PhiSpel <philipp.spelten@web.de>
2025-04-13 10:32:58 +0200

    renamed l2plot and added README

--- examples/noise/gausspulse3d/l2plot.py
+++ examples/noise/gausspulse3d/l2plot.py
@@ -5,17 +5,17 @@ from glob import glob
 plt.rcParams['text.usetex'] = True
 
 fig, ax = plt.subplots(figsize=(6,3), dpi=300)
-filename = glob("tmp_eternal_*_res101*/gnuplotData/data/l2_absolute.dat")[0]
+filename = glob("tmp_eternal_*/gnuplotData/data/l2_absolute.dat")[0]
 data = np.loadtxt(filename)
 ax.plot(data[:,0], data[:,1], label="Idealfall (vergrößerte Domäne)")
-filename = glob("tmp_periodic_*_res101*/gnuplotData/data/l2_absolute.dat")[0]
+filename = glob("tmp_periodic_*/gnuplotData/data/l2_absolute.dat")[0]
 data = np.loadtxt(filename)
 ax.plot(data[:,0], data[:,1], label="Periodische Randbedingungen")
-filename = glob("tmp_local_*_res101*/gnuplotData/data/l2_absolute.dat")[0]
+filename = glob("tmp_local_*/gnuplotData/data/l2_absolute.dat")[0]
 data = np.loadtxt(filename)
 ax.plot(data[:,0], data[:,1], label="Lokale Randbedingungen")
-for bd in [5, 10, 20, 30, 50]:
-    filename = glob("tmp_damping_*_res101_bd" + str(bd) + "*/gnuplotData/data/l2_absolute.dat")[0]
+for bd in [20]:
+    filename = glob("tmp_damping_*_damping" + str(bd) + "*/gnuplotData/data/l2_absolute.dat")[0]
     data = np.loadtxt(filename)
     ax.plot(data[:,0], data[:,1], label=f"Dämpfungsschicht {bd} Punkte")
 ax.set_yscale('log')
