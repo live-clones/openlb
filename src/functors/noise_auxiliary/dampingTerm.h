2808e183f6833f10a530f2c894901e2a34d88962
PhiSpel <philipp.spelten@web.de>
2025-04-13 10:33:23 +0200

    update dampingTerm to take boundary Depth in PU

--- src/functors/noise_auxiliary/dampingTerm.h
+++ src/functors/noise_auxiliary/dampingTerm.h
@@ -6,17 +6,15 @@ protected:
   Vector<T,ndim> x_0, Lx;
   int A=4;
   int n=4;
-  T _boundary_depth_pu;
+  T _bdPU;
   T _damping_strength;
-  UnitConverter<T, DESCRIPTOR> _converter;
 public:
-  DampingTerm(UnitConverter<T, DESCRIPTOR> converter, int boundary_depth_lu, Vector<T,ndim> domain_lengths, T damping_strength = 1. )
-      : AnalyticalF<ndim,T,T>(1), _damping_strength(damping_strength), _converter(converter)
+  DampingTerm(T boundaryDepthPU, Vector<T,ndim> domain_lengths, T damping_strength = 1. )
+      : AnalyticalF<ndim,T,T>(1), _damping_strength(damping_strength), _bdPU(boundaryDepthPU)
       {
-        _boundary_depth_pu = converter.getPhysLength(boundary_depth_lu);
         for (size_t d=0; d<ndim; d++) {
           Lx[d] = domain_lengths[d]/2;  // Lx is usually half the domain
-          x_0[d] = Lx[d] - _boundary_depth_pu;  // subtract boundary depth from domain length
+          x_0[d] = Lx[d] - _bdPU;  // subtract boundary depth from domain length
           // x_0[d] /= Lx[d];  // normalize x_0 (Lx is still needed in operator() to normalize x; Lx will be replaced by 1)
         }
       };
@@ -27,7 +25,7 @@ public:
     Vector<T,ndim> x, distance_from_border;
     bool is_boundary = false;
     for (size_t d=0; d<ndim; d++) {
-      x[d] = ( std::abs(input[d]) - x_0[d] ) / _boundary_depth_pu;  // x_0 becomes 0
+      x[d] = ( std::abs(input[d]) - x_0[d] ) / _bdPU;  // x_0 becomes 0
       x[d] = std::max(x[d], T(0));  // if x[d]<0, ignore for X; if x[d]>1, set to one to avoid negative values of sigma
       if ( x[d] > 0 ) {
         is_boundary = true;
@@ -43,8 +41,8 @@ public:
       }
     }
     T sigma;
-    sigma = A * ( ( (std::pow(X, n))*(1-X)*(n+1)*(n+2) ) / std::pow(_boundary_depth_pu,n+2) );  // x_0 left out
-    sigma /= 9.8304 / std::pow(_boundary_depth_pu, 6);  // calculated as max of function (at X=0.8) for A=n=4
+    sigma = A * ( ( (std::pow(X, n))*(1-X)*(n+1)*(n+2) ) / std::pow(_bdPU,n+2) );  // x_0 left out
+    sigma /= 9.8304 / std::pow(_bdPU, 6);  // calculated as max of function (at X=0.8) for A=n=4
     sigma = std::max(sigma, T(0));
     sigma = std::min(sigma, T(1));  // hard set 0<sigma<1
     sigma *= _damping_strength;
