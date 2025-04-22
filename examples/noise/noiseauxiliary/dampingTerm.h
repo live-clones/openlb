4421087491a23af369bb4cbe8bf3451629460923
PhiSpel <philipp.spelten@web.de>
2025-04-22 13:37:47 +0200

    add header and include guards to dampingTerm.h

--- examples/noise/noiseauxiliary/dampingTerm.h
+++ examples/noise/noiseauxiliary/dampingTerm.h
@@ -1,3 +1,36 @@
+/*  Lattice Boltzmann sample, written in C++, using the OpenLB
+ *  library
+ *
+ *  Copyright (C) 2025 Philipp Spelten
+ *  E-mail contact: info@openlb.net
+ *  The most recent release of OpenLB can be downloaded at
+ *  <http://www.openlb.net/>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA  02110-1301, USA.
+*/
+
+/* dampingTerm.h:
+ * The newtonian cooling term suggested in M. Israeli, S.A. Orszag, 
+ * Approximation of radiation boundary conditions,
+ * J. Comput. Phys. 41 (1981) 115–135.
+*/
+
+#ifndef DAMPING_TERM_H
+#define DAMPING_TERM_H
+ 
 namespace olb {
 
 template <unsigned ndim, typename T, typename DESCRIPTOR>
@@ -52,3 +85,5 @@ public:
 };
  
 }
+
+#endif
