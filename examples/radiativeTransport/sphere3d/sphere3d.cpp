8a7e8c40c4566260fc90e96cab72dede9fc2e851
Andreas <andreas.nettekoven@gmx.de>
2025-10-07 14:47:10 +0200

    Step 2-5 and unit converter

--- examples/radiativeTransport/sphere3d/sphere3d.cpp
+++ examples/radiativeTransport/sphere3d/sphere3d.cpp
@@ -43,9 +43,119 @@ using MyCase = Case<
 >;
 
 namespace olb::parameters{
+    struct DOMAIN_L : public descriptors::FIELD_BASE<1> { };
+
+    struct ABSORPTION : public descriptors::FIELD_BASE<1> { };
+    struct SCATTERING : public descriptors::FIELD_BASE<1> { };
+    struct MU_EFF : public descriptors::FIELD_BASE<1> { };
+}
+
+Mesh<MyCase::value_t,MyCase::d> createMesh(MyCase::ParametersD& params) {
+    using T = MyCase::value_t;
+
+    const auto lx = params.get<parameters::DOMAIN_L>();
+    const auto dx = params.get<parameters::PHYS_DELTA_X>();
+
+    Vector<T,3> origin;
+    IndicatorSphere3D<T> sphereAll(origin,  1 * lx);
+
+    Mesh<T,MyCase::d> mesh(sphereAll, dx, 2*singleton::mpi().getSize());
+    mesh.setOverlap(params.get<parameters::OVERLAP>());
+
+    return mesh;
+}
+
+void prepareGeometry(MyCase& myCase){
+    OstreamManager clout(std::cout, "prepraringGeometry");
+    clout << "Prepare Geometry ..." << std::endl;
+
+    using T = MyCase::value_t;
+    auto& geometry = myCase.getGeometry();
+    auto& params = myCase.getParameters();
+
+    const T dx = params.get<parameters::PHYS_DELTA_X>();
+    const T lx = params.get<parameters::DOMAIN_L>();
+
+    Vector<T,3> origin;
+
+    //Domain, Sphere of Size lx
+    IndicatorSphere3D<T> sphereAll(origin,  1 * lx);
+    geometry.rename(0, 2, sphereAll);
+    geometry.rename(2, 1, {1,1,1});
+
+    //Emitter Spehere of 10% domain:l
+    IndicatorSphere3D<T> sphereEm(origin, 0.1 * lx);
+    geometry.rename(1, 3, sphereEm);
+
+    geometry.clean();
+    geometry.innerClean();
+    geometry.checkForErrors();
+
+    geometry.print();
+}
+
+void prepareLattice(MyCase& myCase){
+    OstreamManager clout(std::cout,"prepareLattice");
+    clout << "Prepare Lattice ..." << std::endl;
+
+    using T = MyCase::value_t;
+    auto& geometry = myCase.getGeometry();
+    auto& params = myCase.getParameters();
+
+    auto& Rlattice = myCase.getLattice(Radiation{});
+
+    using RDESCRIPTOR = MyCase::descriptor_t_of<Radiation>;
+
+    Rlattice.setUnitConverter<UnitConverterFromResolutionAndRelaxationTime<T, RDESCRIPTOR>>(
+        (int) params.get<parameters::RESOLUTION>(), // resolution: number of voxels per charPhysL
+        (T)   params.get<parameters::LATTICE_RELAXATION_TIME>(), // latticeRelaxationTime: relaxation time, has to be greater than 0.5!
+        (T)   1.0,  // charPhysLength: reference length of simulation geometry
+        (T)   0.0,  // charPhysVelocity: maximal/highest expected velocity during simulation in __m / s__  || none for radiation
+        (T)   1.0,  // physViscosity: physical kinematic viscosity in __m^2 / s__  || radiative diffusion coefficient D
+        (T)   1.0   // physDensity: physical density in __kg / m^3__
+    );
+
+    const auto& converter = Rlattice.getUnitConverter();
+    converter.print();
 
 }
 
+
 int main(int argc, char* argv[]) {
     initialize(&argc, &argv);
+
+    /// === Step 2: Set Parameters ===
+    MyCase::ParametersD myCaseParameters;
+    {
+        using namespace olb::parameters;
+        myCaseParameters.set<RESOLUTION>(40);
+        myCaseParameters.set<MAX_PHYS_T>(100);
+        myCaseParameters.set<DOMAIN_L>(1.0);
+        myCaseParameters.set<ABSORPTION>(0.5);
+        myCaseParameters.set<SCATTERING>(1.5);
+
+        myCaseParameters.set<LATTICE_RELAXATION_TIME>(1.0);
+        myCaseParameters.set<MU_EFF>([&] { return
+            util::sqrt(3* myCaseParameters.get<ABSORPTION>() * (myCaseParameters.get<ABSORPTION>() + myCaseParameters.get<SCATTERING>()));
+        });
+
+        myCaseParameters.set<PHYS_DELTA_X>([&] { return
+            myCaseParameters.get<DOMAIN_L>() / myCaseParameters.get<RESOLUTION>();
+        });
+
+    }
+    myCaseParameters.fromCLI(argc, argv);
+
+    /// === Step 3: Create Mesh ===
+    Mesh mesh = createMesh(myCaseParameters);
+
+    /// === Step 4: Create Case ===
+    MyCase myCase(myCaseParameters, mesh);
+
+    /// === Step 5: Prepare Geometry ===
+    prepareGeometry(myCase);
+
+    /// === Step 6: Prepare Lattice ===
+    prepareLattice(myCase);
+
 }
