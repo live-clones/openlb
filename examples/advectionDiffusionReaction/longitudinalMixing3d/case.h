60e2d965da4b53f5ad72d32d187d9fa0de2f22fc
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2025-10-09 18:17:59 +0200

    create case.h

--- examples/advectionDiffusionReaction/longitudinalMixing3d/case.h
+++ examples/advectionDiffusionReaction/longitudinalMixing3d/case.h
@@ -45,21 +45,11 @@ namespace olb::parameters {
   struct OUTPUT_INTERVAL : public descriptors::FIELD_BASE<1> {};
   struct ERROR_TIME : public descriptors::FIELD_BASE<1> {};
   struct C_EQ : public descriptors::FIELD_BASE<1> {};
+  struct L2_ERROR : public descriptors::FIELD_BASE<1> {};
+  struct L1_ERROR : public descriptors::FIELD_BASE<1> {};
+  struct LINF_ERROR : public descriptors::FIELD_BASE<1> {};
 
 }
-
-using MOMENTA = momenta::AdvectionDiffusionBulkTuple;
-using MomentaF = typename momenta::Tuple<
-  momenta::SourcedDensity<typename MOMENTA::density>,
-  momenta::FixedVelocityMomentum,
-  typename MOMENTA::stress,
-  typename MOMENTA::definition>;
-using ReactionADBulkDynamics = dynamics::Tuple<T, TDESCRIPTOR, MomentaF, equilibria::FirstOrder, collision::BGK>;
-
-// vectors for error calculation
-std::vector<T> L2Errors;
-std::vector<T> LinfErrors;
-std::vector<T> L1Errors;
 
 /// functor for analytical solution
 template<typename T, typename S>
@@ -70,26 +60,55 @@ private:
 
 public:
 
-  Solution(MyCase& _myCase, std::size_t iT) : AnalyticalF3D<T,S>(1), myCase(_myCase) {
-    t = converter.getPhysTime(iT);
-    u = converter.getCharPhysVelocity();
-  };
+  Solution(MyCase& _myCase, std::size_t iT) : AnalyticalF3D<T,S>(1), myCase(_myCase) {};
+
   bool operator()(T output[], const S input[])
   {
-    T D = physDiffusivity;
-    T x = input[0];
+    using T = MyCase::value_t;
+    auto& lattice = myCase.getLattice(AdvectionDiffusion{});
+    const auto& converter = lattice.getUnitConverter();
+
+    const T t = converter.getPhysTime(iT);
+    const T u = converter.getCharPhysVelocity();
+    const T D = converter.getPhysDiffusivity();
+    const T Ceq = myCase.getParameters().get<parameters::C_EQ>();
+
+    const T x = input[0];
     output[0] = Ceq*( 0.5*erfc((x-u*t)/(2*sqrt(D*t)))
                         + sqrt(u*u*t/(M_PI*D)) * exp(-pow(x-u*t, 2)/(4*D*t))
                         - 0.5*(1+u*x/D+u*u*t/D) * exp(u*x/D) * erfc((x+u*t)/(2*sqrt(D*t))) );
-                        return true;
+
+    return true;
   }
 };
 
+Mesh<MyCase::value_t,MyCase::d> createMesh(MyCase::ParametersD& params) {
+  using T = MyCase::value_t;
+  const T physCharLength = params.get<parameters::PHYS_CHAR_LENGTH>();
+  const T physDeltaX = physCharLength / params.get<parameters::RESOLUTION>();
 
-void prepareGeometry(SuperGeometry <T,3> &superGeometry,
-                     IndicatorF3D <T> &indicator,
-                     AdeUnitConverter <T, TDESCRIPTOR> converter) {
 
+  //minimal length in y,z direction
+  Vector extent(physCharLength, physDeltaX, physDeltaX);
+  Vector origin(0.0, 0.0, 0.0);
+
+  IndicatorCuboid3D <T> cuboid(extent, origin);
+
+  /// Instantiation of an empty cuboidDecomposition
+#ifdef PARALLEL_MODE_MPI
+  const int noOfCuboids = singleton::mpi().getSize();
+#else
+  const int noOfCuboids = 1;
+#endif
+
+  Mesh<T,MyCase::d> mesh(cuboid, physDeltaX, noOfCuboids);
+  mesh.setOverlap(params.get<parameters::OVERLAP>());
+  mesh.getCuboidDecomposition().setPeriodicity({false,true,true});
+  return mesh;
+}
+
+
+void prepareGeometry(MyCase& myCase) {
   // === MATERIAL NUMBERS === //
   // solid: MN = 0            //
   // bulk: MN = 1             //
@@ -99,82 +118,130 @@ void prepareGeometry(SuperGeometry <T,3> &superGeometry,
   // bulk reaction: MN = 5    //
   // ======================== //
 
+  using T = MyCase::value_t;
+  auto& geometry = myCase.getGeometry();
+  auto& lattice = myCase.getLattice(AdvectionDiffusion{});
+  const auto& converter = lattice.getUnitConverter();
+
   OstreamManager clout(std::cout, "prepareGeometry");
   clout << "Prepare Geometry ..." << std::endl;
 
   const T delta = converter.getPhysDeltaX();
+  const T physLength = myCase.getParameters().get<parameters::PHYS_CHAR_LENGTH>();
 
   Vector<T,3> extend( 2*delta, physLength+2*delta, physLength+2*delta );
   Vector<T,3> origin(-delta, -delta, -delta);
 
-  superGeometry.rename( 0,2 );
-  superGeometry.rename( 2,1,{1,0,0} );
-
+  geometry.rename( 0,2 );
+  geometry.rename( 2,1,{1,0,0} );
 
   // Set material number for inflow
   origin[0] = -delta;
   IndicatorCuboid3D<T> inflow( extend, origin );
-  superGeometry.rename( 2,3,inflow );
+  geometry.rename( 2,3,inflow );
 
 #ifdef IN_BULK_APPROACH
   IndicatorCuboid3D<T> layer( extend, origin );
-  superGeometry.rename( 1,5,layer );
+  geometry.rename( 1,5,layer );
 #endif
 
   // Set material number for outflow
   origin[0] = physLength-delta;
   IndicatorCuboid3D<T> outflow( extend, origin );
-  superGeometry.rename( 2,4,outflow );
+  geometry.rename( 2,4,outflow );
 
-  superGeometry.communicate();
-  superGeometry.print();
+  geometry.communicate();
+  geometry.print();
 
   clout << "Prepare Geometry ... OK" << std::endl;
 }
 
 
-void prepareLattice(SuperLattice <T, TDESCRIPTOR> &ADlattice,
-                    SuperGeometry <T,3> &superGeometry,
-                    AdeUnitConverter <T, TDESCRIPTOR> converter) {
+void prepareLattice(MyCase& myCase)
+{
+  using T = MyCase::value_t;
+  using DESCRIPTOR = MyCase::descriptor_t_of<AdvectionDiffusion>;
+  using namespace olb::parameters;
+
+  auto& ADlattice = myCase.getLattice(AdvectionDiffusion{});
+  auto& geometry = myCase.getGeometry();
+  auto& parameters = myCase.getParameters();
+  const auto& converter = ADlattice.getUnitConverter();
+
   OstreamManager clout(std::cout, "prepareLattice");
   clout << "Prepare Lattice ..." << std::endl;
 
   const T omega = converter.getLatticeRelaxationFrequency();
+  const T N = converter.getResolution();
+  const T relaxationTime = converter.getLatticeRelaxationTime();
+  const T physLength = converter.getPhysLength();
+  const T Pe = converter.getPecletNumber();
+  const T physDiffusivity = converter.getPhysDiffusivity();
+  const T physDensity = converter.getPhysDensity();
+
+  ADlattice.setUnitConverter<AdeUnitConverterFromResolutionAndRelaxationTime<T,DESCRIPTOR>>(
+      N,                              // Resolution
+      relaxationTime,                 // Relaxation Time
+      physLength,                     // charPhysLength
+      Pe*physDiffusivity/physLength,  // charPhysVelocity from peclet
+      physDiffusivity,                // physDiffusivity
+      physDensity                     // physDensity
+  );
+  converter.print();
 
 #ifdef BOUNDARY_CONDITION_APPROACH
-  auto bulkIndicator = superGeometry.getMaterialIndicator({1,3,4});
+  auto bulkIndicator = geometry.getMaterialIndicator({1,3,4});
   ADlattice.defineDynamics<AdvectionDiffusionBGKdynamics>(bulkIndicator);
   // reactive Robin boundary at left side MN=3
-  boundary::set<boundary::Robin>(ADlattice, superGeometry.getMaterialIndicator({3}));
+  boundary::set<boundary::Robin>(ADlattice, geometry.getMaterialIndicator({3}));
   T reactionRate = converter.getCharLatticeVelocity(); //determines speed of reaction, here equal to velocity
   AnalyticalConst3D <T,T> coefficients(reactionRate, -converter.getLatticeDiffusivity(), reactionRate * Ceq); //set robin coefficients as found in the paper
-  ADlattice.template defineField<descriptors::G>(superGeometry.getMaterialIndicator({3}), (coefficients)); //save coefficient in a field
+  ADlattice.template defineField<descriptors::G>(geometry.getMaterialIndicator({3}), (coefficients)); //save coefficient in a field
 
   // ZeroGradient using RobinBoundary at right side MN=4
-  boundary::set<boundary::Robin>(ADlattice, superGeometry.getMaterialIndicator({4}));
+  boundary::set<boundary::Robin>(ADlattice, geometry.getMaterialIndicator({4}));
   AnalyticalConst3D <T,T> coefficients2(0., 1., 0.);
-  ADlattice.template defineField<descriptors::G>(superGeometry.getMaterialIndicator({4}), (coefficients2));
+  ADlattice.template defineField<descriptors::G>(geometry.getMaterialIndicator({4}), (coefficients2));
 #endif
 
 
 #ifdef IN_BULK_APPROACH
-  auto bulkIndicator = superGeometry.getMaterialIndicator({1,3,4,5});
+  auto bulkIndicator = geometry.getMaterialIndicator({1,3,4,5});
   ADlattice.defineDynamics<SourcedAdvectionDiffusionBGKdynamics>(bulkIndicator);
   // BounceBack for MN=3
-  boundary::set<boundary::BounceBack><T, TDESCRIPTOR>(ADlattice, superGeometry.getMaterialIndicator({3}));
+  boundary::set<boundary::BounceBack><T, TDESCRIPTOR>(ADlattice, geometry.getMaterialIndicator({3}));
   // Zero Gradient at right side MN=4
-  setZeroGradientBoundary<T, TDESCRIPTOR>(ADlattice, superGeometry.getMaterialIndicator({4}));
+  setZeroGradientBoundary<T, TDESCRIPTOR>(ADlattice, geometry.getMaterialIndicator({4}));
   // constants used in defineField
   AnalyticalConst3D<T,T> mat1(1.);
   AnalyticalConst3D<T,T> mat0(0.);
   // necessary for Zero Gradient Boundary
-  ADlattice.template defineField<descriptors::SCALAR>(superGeometry.getMaterialIndicator({1,4}), mat1 );
-  ADlattice.template defineField<descriptors::SCALAR>(superGeometry.getMaterialIndicator({0,3,5}), mat0 );
+  ADlattice.template defineField<descriptors::SCALAR>(geometry.getMaterialIndicator({1,4}), mat1 );
+  ADlattice.template defineField<descriptors::SCALAR>(geometry.getMaterialIndicator({0,3,5}), mat0 );
   // use field GAMMA to define where reaction is allowed to happen
-  ADlattice.template defineField<descriptors::GAMMA>(superGeometry.getMaterialIndicator({5}), mat1 );
-  ADlattice.template defineField<descriptors::GAMMA>(superGeometry.getMaterialIndicator({0,1,3,4}), mat0 );
+  ADlattice.template defineField<descriptors::GAMMA>(geometry.getMaterialIndicator({5}), mat1 );
+  ADlattice.template defineField<descriptors::GAMMA>(geometry.getMaterialIndicator({0,1,3,4}), mat0 );
 #endif
 
+  clout << "Prepare Lattice ... OK" << std::endl;
+}
+
+void setInitialValues(MyCase& myCase) {
+  using T = MyCase::value_t;
+  auto& ADlattice = myCase.getLattice(AdvectionDiffusion{});
+  auto& geometry = myCase.getGeometry();
+  const auto& converter = ADlattice.getUnitConverter();
+  auto& parameters = myCase.getParameters();
+
+  const T omega = converter.getLatticeRelaxationFrequency();
+
+  #ifdef BOUNDARY_CONDITION_APPROACH
+    const auto bulkIndicator = geometry.getMaterialIndicator({1,3,4});
+  #endif
+
+  #ifdef IN_BULK_APPROACH
+    const auto bulkIndicator = geometry.getMaterialIndicator({1,3,4,5});
+  #endif
 
   // Initial advection velocity and concentration
   AnalyticalConst3D <T, T> u0(converter.getCharLatticeVelocity(), 0.0, 0.0);
@@ -186,69 +253,76 @@ void prepareLattice(SuperLattice <T, TDESCRIPTOR> &ADlattice,
 
   ADlattice.setParameter<descriptors::OMEGA>( omega );
   ADlattice.initialize();
-  clout << "Prepare Lattice ... OK" << std::endl;
 }
 
 
 // compute absolute and relative errors with different norms
-void error(SuperLattice<T, TDESCRIPTOR>& ADlattice,
-           SuperGeometry<T,3>& superGeometry,
-           std::size_t iT,
-           AdeUnitConverter <T, TDESCRIPTOR> converter) {
+void errorEval( MyCase& myCase, std::size_t iT)
+{
+  using T = MyCase::value_t;
+  using DESCRIPTOR = MyCase::descriptor_t_of<AdvectionDiffusion>;
+  auto& ADlattice = myCase.getLattice(AdvectionDiffusion{});
+  auto& geometry = myCase.getGeometry();
+  const auto& converter = ADlattice.getUnitConverter();
+  auto& parameters = myCase.getParameters();
+
   OstreamManager clout(std::cout, "error");
 
   T result[3] = {T(), T(), T()};
   int tmp[] = {int()};
 
-  SuperLatticeDensity3D <T, TDESCRIPTOR> numericalConc(ADlattice);
-  Solution<T,T> analyticalConc(converter, iT);
+  SuperLatticeDensity3D <T, DESCRIPTOR> numericalConc(ADlattice);
+  Solution<T,T> analyticalConc(myCase, iT);
 
 #ifdef IN_BULK_APPROACH
-  auto indicatorF = superGeometry.getMaterialIndicator({1,5}); // MN 3 and 4 should not be included for IN_BULK
+  auto indicatorF = geometry.getMaterialIndicator({1,5}); // MN 3 and 4 should not be included for IN_BULK
 #endif
 #ifdef BOUNDARY_CONDITION_APPROACH
-  auto indicatorF = superGeometry.getMaterialIndicator({1,3,4});
+  auto indicatorF = geometry.getMaterialIndicator({1,3,4});
 #endif
 
-  SuperRelativeErrorL2Norm3D <T> relTemperatureErrorL2Norm(numericalConc, analyticalConc, indicatorF);
+  SuperRelativeErrorL2Norm3D<T> relTemperatureErrorL2Norm(numericalConc, analyticalConc, indicatorF);
   SuperRelativeErrorL1Norm3D<T> relTemperatureErrorL1Norm(numericalConc, analyticalConc, indicatorF);
   SuperRelativeErrorLinfNorm3D<T> relTemperatureErrorLinfNorm(numericalConc, analyticalConc, indicatorF);
 
   /// Console output of every error norm
   relTemperatureErrorL2Norm(result, tmp);
-  L2Errors.push_back(result[0]);
+  parameters.set<parameters::L2_ERROR>(result[0]);
   relTemperatureErrorL1Norm(result, tmp);
-  L1Errors.push_back(result[0]);
+  parameters.set<parameters::L1_ERROR>(result[0]);
   relTemperatureErrorLinfNorm(result, tmp);
-  LinfErrors.push_back(result[0]);
+  parameters.set<parameters::LINF_ERROR>(result[0]);
 }
 
+void getResults(MyCase& myCase, const std::size_t iT,
+                util::Timer <MyCase::value_t> &timer)
+{
+  using T = MyCase::value_t;
+  using DESCRIPTOR = MyCase::descriptor_t_of<AdvectionDiffusion>;
+  auto& ADlattice = myCase.getLattice(AdvectionDiffusion{});
+  const auto& converter = ADlattice.getUnitConverter();
+  auto& parameters = myCase.getParameters();
 
-
-void getResults(SuperLattice<T, TDESCRIPTOR> &ADlattice,
-                std::size_t iT,
-                AdeUnitConverter <T, TDESCRIPTOR> converter,
-                SuperGeometry<T,3> &superGeometry,
-                util::Timer <T> &timer) {
+  const T statIter = parameters.get<parameters::OUTPUT_INTERVAL>();
+  const T errorTime = parameters.get<parameters::ERROR_TIME>();
 
   OstreamManager clout(std::cout, "getResults");
   SuperVTMwriter3D <T> vtkWriter("longitudinalMixing3d");
 
-  SuperLatticeDensity3D <T, TDESCRIPTOR> concentration(ADlattice);
+  SuperLatticeDensity3D <T, DESCRIPTOR> concentration(ADlattice);
   vtkWriter.addFunctor( concentration );
   concentration.getName() = "concentration";
 
-  Solution<T,T> analyticalSolution(converter, iT);
-  SuperLatticeFfromAnalyticalF3D<T, TDESCRIPTOR> functor(analyticalSolution, ADlattice);
+  Solution<T,T> analyticalSolution(myCase, iT);
+  SuperLatticeFfromAnalyticalF3D<T, DESCRIPTOR> functor(analyticalSolution, ADlattice);
   functor.getName() = "analyticalSolution";
   vtkWriter.addFunctor( functor );
-
 
   if (iT == 0) {
     /// Writes the geometry, cuboid no. and rank no. as vti file for visualization
 
-    SuperLatticeCuboid3D <T, TDESCRIPTOR> cuboid(ADlattice);
-    SuperLatticeRank3D <T, TDESCRIPTOR> rank(ADlattice);
+    SuperLatticeCuboid3D <T, DESCRIPTOR> cuboid(ADlattice);
+    SuperLatticeRank3D <T, DESCRIPTOR> rank(ADlattice);
     vtkWriter.write(cuboid);
     vtkWriter.write(rank);
 
@@ -268,80 +342,53 @@ void getResults(SuperLattice<T, TDESCRIPTOR> &ADlattice,
     vtkWriter.write(iT);
   }
   if(iT == converter.getLatticeTime(errorTime)){
-    error(ADlattice, superGeometry, iT, converter);
+    errorEval(myCase, iT);
   }
 }
 
 
-void simulate(int N) {
+void simulate(MyCase& myCase)
+{
+  using T = MyCase::value_t;
+  auto& ADlattice = myCase.getLattice(AdvectionDiffusion{});
+  auto& geometry = myCase.getGeometry();
+  const auto& converter = ADlattice.getUnitConverter();
+  auto& parameters = myCase.getParameters();
+
+  const T N = converter.getResolution();
+  const T Ceq = parameters.get<parameters::C_EQ>();
+  const T maxPhysT = parameters.get<parameters::MAX_PHYS_T>();
+  const T runs = parameters.get<parameters::RUNS>();
 
   OstreamManager clout(std::cout, "simulate");
   clout << "Executing the simulation with N=" << std::to_string(N) << std::endl;
-
-  AdeUnitConverterFromResolutionAndRelaxationTime<T,TDESCRIPTOR> converter(
-      N,                              // Resolution
-      relaxationTime,                 // Relaxation Time
-      physLength,                     // charPhysLength
-      Pe*physDiffusivity/physLength,  // charPhysVelocity from peclet
-      physDiffusivity,                // physDiffusivity
-      1                               // physDensity
-  );
-  converter.print();
 
   // change directory for different runs
   if (runs > 1) {
     singleton::directories().setOutputDir("./tmp/N" + std::to_string(N) + "/");
   }
 
-  /// === 2nd Step: Prepare Geometry ===
-  Vector<T,3> extend( physLength, converter.getPhysDeltaX(), converter.getPhysDeltaX() ); //minimal length in y,z direction
-  Vector<T,3> origin( 0.0, 0.0, 0.0 );
-
-  IndicatorCuboid3D <T> cuboid(extend, origin);
-
-  /// Instantiation of an empty cuboidDecomposition
-#ifdef PARALLEL_MODE_MPI
-  const int noOfCuboids = singleton::mpi().getSize();
-#else
-  const int noOfCuboids = 1;
-#endif
-  CuboidDecomposition3D <T> cuboidDecomposition(cuboid, converter.getPhysDeltaX(), noOfCuboids);
-  cuboidDecomposition.setPeriodicity({false, true, true});
-
-  /// Instantiation of a loadBalancer
-  HeuristicLoadBalancer <T> loadBalancer(cuboidDecomposition);
-
-  /// Instantiation of a superGeometry
-  SuperGeometry<T,3> superGeometry(cuboidDecomposition, loadBalancer, 2);
-  prepareGeometry(superGeometry, cuboid, converter);
-
-  /// === 3rd Step: Prepare Lattice ===
-  SuperLattice<T, TDESCRIPTOR> ADlattice(converter, superGeometry);
-  prepareLattice(ADlattice, superGeometry, converter);
-
-  // Prepare reaction at left wall for IN_BULK_APPROACH
-  SuperLatticeCoupling Reaction(
-      LongitudinalMixingReactionCoupling<T>{},
-      names::Concentration0{}, ADlattice);
-  Reaction.template setParameter<typename LongitudinalMixingReactionCoupling<T>::
-    REACTION_CONSTANT>({converter.getCharLatticeVelocity()}); //determines speed of reaction, here equal to velocity
-  Reaction.template setParameter<typename LongitudinalMixingReactionCoupling<T>::EQUILIBRIUM>({Ceq}); //equilibrium concentration
-
-
-  /// === 4th Step: Main Loop with Timer ===
-  util::Timer<T> timer(converter.getLatticeTime(maxPhysT), superGeometry.getStatistics().getNvoxel());
+  util::Timer<T> timer(converter.getLatticeTime(maxPhysT), geometry.getStatistics().getNvoxel());
   timer.start();
 
   for (std::size_t iT = 0; iT < converter.getLatticeTime(maxPhysT); ++iT) {
 
-    /// === 6th Step: Collide and Stream Execution ===
 #ifdef IN_BULK_APPROACH
+    // Prepare reaction at left wall for IN_BULK_APPROACH
+    SuperLatticeCoupling Reaction(
+      LongitudinalMixingReactionCoupling<T>{},
+      names::Concentration0{}, ADlattice);
+    Reaction.template setParameter<typename LongitudinalMixingReactionCoupling<T>::
+      REACTION_CONSTANT>({converter.getCharLatticeVelocity()}); //determines speed of reaction, here equal to velocity
+    Reaction.template setParameter<typename LongitudinalMixingReactionCoupling<T>::EQUILIBRIUM>({Ceq}); //equilibrium concentration
+
     Reaction.apply();
 #endif
     ADlattice.collideAndStream();
-    // === 7th Step: Computation and Output of the Results ===
-    getResults( ADlattice, iT, converter, superGeometry, timer );
+
+    getResults( myCase, iT, timer );
   }
+
   timer.stop();
   timer.printSummary();
 }
