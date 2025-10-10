7e57f10359b07a3f4bf93343c4d53627f719f46c
Adrian Kummerlaender <adrian.kummerlaender@kit.edu>
2025-10-10 19:34:53 +0200

    examples: Resolve duplicate parameters

--- examples/laminar/nonNewtonianPoiseuille3d/case.h
+++ examples/laminar/nonNewtonianPoiseuille3d/case.h
@@ -62,7 +62,6 @@ namespace olb::parameters {
   struct RADIUS             : public descriptors::FIELD_BASE<1> {};
   struct EOC                : public descriptors::TYPED_FIELD_BASE<bool,1> {};
   struct VISCOSITY_MODEL    : public descriptors::TYPED_FIELD_BASE<ViscosityModel,1> {};
-  struct ORIGIN             : public descriptors::FIELD_BASE<0,1> {};
   struct AXIS               : public descriptors::FIELD_BASE<0,1> {};
   struct EOC_RESOLUTIONS    : public descriptors::TYPED_FIELD_BASE<int,4> {};
 
