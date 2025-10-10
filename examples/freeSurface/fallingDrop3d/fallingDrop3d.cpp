fcc938e8153e3c0a8effc066833bc3a8a4ba6a41
Maximilian Schecher <maximilianschecher@gmail.com>
2025-10-10 11:07:01 +0200

    Renamed parameters to align with the other freeSurface cases. Eliminated white spaces.

--- examples/freeSurface/fallingDrop3d/fallingDrop3d.cpp
+++ examples/freeSurface/fallingDrop3d/fallingDrop3d.cpp
@@ -35,8 +35,6 @@ using namespace olb;
 using namespace olb::names;
 using namespace olb::descriptors;
 
-
-
 // === Step 1: Declarations ===
 using MyCase = Case<
   NavierStokes, Lattice<double, descriptors::D3Q27<descriptors::FORCE,
@@ -49,23 +47,10 @@ using MyCase = Case<
   FreeSurface::HAS_INTERFACE_NBRS>>
 >;
 
-
-namespace olb::parameters {
-//struct GRAVITY                      : public descriptors::FIELD_BASE<0,1> { };
-struct INITIAL_VELOCITY             : public descriptors::FIELD_BASE<0,1> { };
-struct HAS_SURFACE_TENSION          : public descriptors::TYPED_FIELD_BASE<bool, 1> {};
-struct SURFACE_TENSION_COEFFICIENT  : public descriptors::FIELD_BASE<1> { };
-struct TRANSITION_THRESHOLD         : public descriptors::FIELD_BASE<1> { };
-struct LONELY_THRESHOLD             : public descriptors::FIELD_BASE<1> { };
-}
-
-
-
 /// @brief Create a simulation mesh, based on user-specified geometry
 /// @return An instance of a mesh with the relevant information
 Mesh<MyCase::value_t, MyCase::d> createMesh(MyCase::ParametersD& parameters) {
   using T = MyCase::value_t;
-  using DESCRIPTOR = MyCase::descriptor_t_of<NavierStokes>;
 
   Vector<T,3> extend(parameters.get<parameters::DOMAIN_EXTENT>()[0], parameters.get<parameters::DOMAIN_EXTENT>()[1],parameters.get<parameters::DOMAIN_EXTENT>()[2]);
   Vector<T,3> origin;
@@ -125,6 +110,7 @@ class FreeSurfaceFallingDrop3D final : public AnalyticalF3D<T, T> {
     }
 };
 
+/// @brief Define the initial state of the falling drop
 template <typename T, typename DESCRIPTOR>
 class FallingDropVel3D final : public AnalyticalF<3, T, T> {
 private:
@@ -134,7 +120,6 @@ public:
     FallingDropVel3D(T lattice_size_, const std::array<T, DESCRIPTOR::d>& lattice_speed_) :AnalyticalF<3, T, T>{ 3 }, lattice_size{ lattice_size_ }, lattice_speed{ lattice_speed_ }{}
 
     bool operator()(T output[], const T x[]) override {
-
 
         T radius = 0.00155;
         std::array<T, DESCRIPTOR::d> point = { 0.015, 0.015, 2 * radius + lattice_size * 2 };
@@ -160,7 +145,10 @@ public:
         return true;
     }
 };
-//SuperGeometry<T, 3>& superGeometry
+
+/// @brief Set material numbers for different parts of the domain
+/// @param myCase The Case instance which keeps the simulation data
+/// @note The material numbers will be used to assign physics to lattice nodes
 void prepareGeometry(MyCase& myCase) {
 
     OstreamManager clout(std::cout, "prepareGeometry");
@@ -179,29 +167,26 @@ void prepareGeometry(MyCase& myCase) {
 
     clout << "Prepare Geometry ... OK" << std::endl;
 }
-/*UnitConverter<T, DESCRIPTOR> const& converter,
-    SuperLattice<T, DESCRIPTOR>& sLattice,
-    SuperGeometry<T, 3>& superGeometry, T lattice_size*/
+
+/// @brief Set up the falling drop with the freeSurface information
 void prepareFallingDrop(MyCase& myCase)
 {
-  
-  
   using DESCRIPTOR = MyCase::descriptor_t_of<NavierStokes>;
   using T = MyCase::value_t;
-  
   auto& lattice = myCase.getLattice(NavierStokes{});
   auto& geometry = myCase.getGeometry();
   auto& parameters = myCase.getParameters();
-  const T char_phys_length = parameters.get<parameters::PHYS_CHAR_LENGTH>();
+
+  const T char_phys_length = parameters.get<parameters::DOMAIN_EXTENT>()[0];
   const int resolution = parameters.get<parameters::RESOLUTION>();
   std::array<T,3> gravity_force;
   gravity_force[0] = parameters.get<parameters::GRAVITY>()[0];
   gravity_force[1] = parameters.get<parameters::GRAVITY>()[1];
   gravity_force[2] = parameters.get<parameters::GRAVITY>()[2];
   auto& converter = lattice.getUnitConverter();
-  // prepareFallingDrop(...);
+
   const T lattice_size = char_phys_length / resolution;
-  
+
   AnalyticalConst3D<T, T> zero(0.);
   AnalyticalConst3D<T, T> one(1.);
   AnalyticalConst3D<T, T> two(2.);
@@ -218,25 +203,25 @@ void prepareFallingDrop(MyCase& myCase)
     lattice.defineField<FreeSurface::CELL_FLAGS>(geometry, i, zero);
     lattice.defineField<descriptors::FORCE>(geometry, i, force_zero);
     lattice.defineField<FreeSurface::PREVIOUS_VELOCITY>(geometry, i, force_zero);
-    }
+  }
 
-    lattice.defineField<FreeSurface::CELL_TYPE>(geometry, 1, cells_analytical);
-    lattice.defineField<FreeSurface::MASS>(geometry, 1, mass_analytical);
-    lattice.defineField<FreeSurface::EPSILON>(geometry, 1, mass_analytical);
+  lattice.defineField<FreeSurface::CELL_TYPE>(geometry, 1, cells_analytical);
+  lattice.defineField<FreeSurface::MASS>(geometry, 1, mass_analytical);
+  lattice.defineField<FreeSurface::EPSILON>(geometry, 1, mass_analytical);
 
   for (int i : {0, 2}) {
     lattice.defineField<FreeSurface::EPSILON>(geometry, i, one);
     lattice.defineField<FreeSurface::CELL_TYPE>(geometry, i, four);
-    }
+  }
 
-    T force_factor = T(1) / converter.getConversionFactorForce() * converter.getConversionFactorMass();
-    AnalyticalConst3D<T, T> force_a{ gravity_force[0] * force_factor, gravity_force[1] * force_factor, gravity_force[2] * force_factor };
-    lattice.defineField<descriptors::FORCE>(geometry.getMaterialIndicator({ 1 }), force_a);
+  T force_factor = T(1) / converter.getConversionFactorForce() * converter.getConversionFactorMass();
+  AnalyticalConst3D<T, T> force_a{ gravity_force[0] * force_factor, gravity_force[1] * force_factor, gravity_force[2] * force_factor };
+  lattice.defineField<descriptors::FORCE>(geometry.getMaterialIndicator({ 1 }), force_a);
 }
 
+/// @brief Set lattice dynamics
+/// @param myCase The Case instance which keeps the simulation data
 void prepareLattice(MyCase& myCase) {
-
-
   using DESCRIPTOR = MyCase::descriptor_t_of<NavierStokes>;
   using T = MyCase::value_t;
   auto& lattice = myCase.getLattice(NavierStokes{});
@@ -245,97 +230,96 @@ void prepareLattice(MyCase& myCase) {
 
   OstreamManager clout(std::cout, "prepareLattice");
   clout << "Prepare Lattice ..." << std::endl;
-  
+
   const int resolution = parameters.get<parameters::RESOLUTION>();
   const T latticeRelaxationTime = parameters.get<parameters::LATTICE_RELAXATION_TIME>();
   const T char_phys_length = parameters.get<parameters::DOMAIN_EXTENT>()[0];
   const T char_phys_vel = parameters.get<parameters::PHYS_CHAR_VELOCITY>();
   const T viscosity = parameters.get<parameters::PHYS_CHAR_VISCOSITY>();
   const T density = parameters.get<parameters::PHYS_CHAR_DENSITY>();
-  const T lattice_size = char_phys_length / resolution;
-  
-  lattice.setUnitConverter < UnitConverterFromResolutionAndRelaxationTime<T, DESCRIPTOR>>(
-    int{ resolution },     // resolution: number of voxels per charPhysL
-    (T)latticeRelaxationTime,   // latticeRelaxationTime: relaxation time, have to be greater than 0.5!
-    (T)char_phys_length,     // charPhysLength: reference length of simulation geometry
-    (T)char_phys_vel,     // charPhysVelocity: maximal/highest expected velocity during simulation in __m / s__
-    (T)viscosity, // physViscosity: physical kinematic viscosity in __m^2 / s__
-    (T)density     // physDensity: physical density in __kg / m^3__
-    );
-  auto& converter = lattice.getUnitConverter();
-    
 
-    // Material=1 -->bulk dynamics
+  lattice.setUnitConverter < UnitConverterFromResolutionAndRelaxationTime<T, DESCRIPTOR>>(
+    int{ resolution },          // resolution: number of voxels per charPhysL
+    (T)latticeRelaxationTime,   // latticeRelaxationTime: relaxation time, have to be greater than 0.5!
+    (T)char_phys_length,        // charPhysLength: reference length of simulation geometry
+    (T)char_phys_vel,           // charPhysVelocity: maximal/highest expected velocity during simulation in __m / s__
+    (T)viscosity,               // physViscosity: physical kinematic viscosity in __m^2 / s__
+    (T)density                  // physDensity: physical density in __kg / m^3__
+  );
+  auto& converter = lattice.getUnitConverter();
+
+  // Material=1 -->bulk dynamics
   lattice.defineDynamics<SmagorinskyForcedBGKdynamics<T, DESCRIPTOR>>(geometry, 1);
-    // Material=2 -->no-slip boundary
+  // Material=2 -->no-slip boundary
   lattice.defineDynamics<BounceBack<T, DESCRIPTOR>>(geometry, 2);
-    //setSlipBoundary<T,DESCRIPTOR>(sLattice, superGeometry, 2);
+  //setSlipBoundary<T,DESCRIPTOR>(sLattice, superGeometry, 2);
 
   lattice.setParameter<descriptors::OMEGA>(converter.getLatticeRelaxationFrequency());
   lattice.setParameter<collision::LES::SMAGORINSKY>(T(0.2));
 
   prepareFallingDrop(myCase);
-  T transitionThreshold = parameters.get<parameters::TRANSITION_THRESHOLD>();
-  T lonelyThreshold = parameters.get<parameters::LONELY_THRESHOLD>();
-  bool has_surface_tension = parameters.get<parameters::HAS_SURFACE_TENSION>();
-  T surface_tension = parameters.get<parameters::SURFACE_TENSION_COEFFICIENT>();
+
+  const bool drop_isolated_cells = parameters.get<FreeSurface::DROP_ISOLATED_CELLS>();
+  const bool has_surface_tension = parameters.get<FreeSurface::HAS_SURFACE_TENSION>();
+  const T surface_tension_coefficient = parameters.get<FreeSurface::SURFACE_TENSION_PARAMETER>();
+  const T transitionThreshold = parameters.get<FreeSurface::TRANSITION>();
+  const T lonelyThreshold = parameters.get<FreeSurface::LONELY_THRESHOLD>();
 
   // Compute the scaling factor for surface tension
-  
-  T surface_tension_factor =
-      std::pow(converter.getConversionFactorTime(), 2) /
-      (density * std::pow(converter.getPhysDeltaX(), 3));
+  T surface_tension_factor = std::pow(converter.getConversionFactorTime(), 2) / (density * std::pow(converter.getPhysDeltaX(), 3));
 
   static FreeSurface3DSetup<T, DESCRIPTOR> free_surface_setup{ lattice };
   free_surface_setup.addPostProcessor();
 
-  lattice.setParameter<FreeSurface::DROP_ISOLATED_CELLS>(true);
+  lattice.setParameter<FreeSurface::DROP_ISOLATED_CELLS>(drop_isolated_cells);
   lattice.setParameter<FreeSurface::TRANSITION>(transitionThreshold);
   lattice.setParameter<FreeSurface::LONELY_THRESHOLD>(lonelyThreshold);
   lattice.setParameter<FreeSurface::HAS_SURFACE_TENSION>(has_surface_tension);
-  lattice.setParameter<FreeSurface::SURFACE_TENSION_PARAMETER>(
-      surface_tension_factor * surface_tension);
-    clout << "Prepare Lattice ... OK" << std::endl;
+  lattice.setParameter<FreeSurface::SURFACE_TENSION_PARAMETER>(surface_tension_factor * surface_tension_coefficient);
+  clout << "Prepare Lattice ... OK" << std::endl;
 }
 
-void setInitialValues(MyCase& myCase){
 
+/// Set initial condition for primal variables (velocity and density)
+/// @param myCase The Case instance which keeps the simulation data
+/// @note Be careful: initial values have to be set using lattice units
+void setInitialValues(MyCase& myCase){
   using DESCRIPTOR = MyCase::descriptor_t_of<NavierStokes>;
   using T = MyCase::value_t;
   auto& lattice = myCase.getLattice(NavierStokes{});
   auto& geometry = myCase.getGeometry();
   auto& parameters = myCase.getParameters();
   auto& converter = lattice.getUnitConverter();
-  
+
   OstreamManager clout( std::cout,"setInitialValues" );
 
   std::array<T,3> initial_falling_speed;
-  initial_falling_speed[0] = parameters.get<parameters::INITIAL_VELOCITY>()[0];
-  initial_falling_speed[1] = parameters.get<parameters::INITIAL_VELOCITY>()[1];
-  initial_falling_speed[2] = parameters.get<parameters::INITIAL_VELOCITY>()[2];
+  initial_falling_speed[0] = parameters.get<parameters::INITIAL_FALLING_SPEED>()[0];
+  initial_falling_speed[1] = parameters.get<parameters::INITIAL_FALLING_SPEED>()[1];
+  initial_falling_speed[2] = parameters.get<parameters::INITIAL_FALLING_SPEED>()[2];
 
   std::array<T, DESCRIPTOR::d> lattice_speed;
-    for (size_t i = 0; i < DESCRIPTOR::d; ++i) {
-        lattice_speed[i] = initial_falling_speed[i] * converter.getPhysDeltaT() / converter.getPhysDeltaX();
-    }
+  for (size_t i = 0; i < DESCRIPTOR::d; ++i) {
+      lattice_speed[i] = initial_falling_speed[i] * converter.getPhysDeltaT() / converter.getPhysDeltaX();
+  }
 
   T characteristic_length = parameters.get<parameters::DOMAIN_EXTENT>()[0];
   T lattice_size = characteristic_length / parameters.get<parameters::RESOLUTION>();
-  
+
   FallingDropVel3D<T, DESCRIPTOR> u{ lattice_size, lattice_speed };
   AnalyticalConst3D<T, T> one(1.);
 
   lattice.defineRhoU(geometry.getMaterialIndicator({ 0,1,2 }), one, u);
   for (int i : {0, 1, 2}) {
         lattice.iniEquilibrium(geometry, i, one, u);
-    }
+  }
 
-    // Set up free surface communicator stages
+  // Set up free surface communicator stages
   FreeSurface::initialize(lattice);
-    // Make the lattice ready for simulation
+
+  // Make the lattice ready for simulation
   lattice.initialize();
-    
-    
+
 }
 
 /// Update boundary values at times (and external fields, if they exist)
@@ -348,9 +332,10 @@ void setTemporalValues(MyCase& myCase,
   // Nothing to do here, because simulation does not depend on time
 }
 
-/*SuperLattice<T, DESCRIPTOR>& sLattice,
-    UnitConverter<T, DESCRIPTOR> const& converter, int iT,
-    SuperGeometry<T, 3>& superGeometry, util::Timer<T>& timer*/
+
+/// Compute simulation results at times
+/// @param myCase The Case instance which keeps the simulation data
+/// @param iT The time step, timer
 void getResults(MyCase& myCase, int iT, util::Timer<MyCase::value_t>& timer)
 {
   using DESCRIPTOR = MyCase::descriptor_t_of<NavierStokes>;
@@ -416,11 +401,10 @@ void simulate( MyCase& myCase ){
   auto& geometry = myCase.getGeometry();
   auto& converter = lattice.getUnitConverter();
   auto& parameters = myCase.getParameters();
-  T characteristic_length = parameters.get<parameters::DOMAIN_EXTENT>()[0];
-  T lattice_size = characteristic_length / parameters.get<parameters::RESOLUTION>();
+
   // Main Loop with Timer
   OstreamManager clout( std::cout,"starting simulation..." );
-  
+
   T physTime = parameters.get<parameters::MAX_PHYS_T>();
     util::Timer<T> timer(converter.getLatticeTime(physTime), geometry.getStatistics().getNvoxel());
     timer.start();
@@ -436,18 +420,12 @@ void simulate( MyCase& myCase ){
 
 }
 
+/// Setup and run a simulation
 int main(int argc, char** argv)
 {
+  initialize(&argc, &argv, false, false);
 
-    initialize(&argc, &argv, false, false);
-
-    // FreeSurfaceConfig c;
-    //OstreamManager clerr(std::cerr, "main");
-    //OstreamManager clout(std::cout, "main");
-
-    //singleton::directories().setOutputDir("./tmp/");
-    
-    /// === Step 2: Set Parameters ===
+  /// === Step 2: Set Parameters ===
   MyCase::ParametersD myCaseParameters;
   {
     using namespace olb::parameters;
@@ -458,24 +436,17 @@ int main(int argc, char** argv)
     myCaseParameters.set<RESOLUTION>(100); // resolution: number of voxels per charPhysL
     myCaseParameters.set<DOMAIN_EXTENT>({0.03, 0.03, 0.025}); //formerly area
     myCaseParameters.set<GRAVITY>({0., 0., -9.81});
-    myCaseParameters.set<PHYS_CHAR_LENGTH>(0.03); //char_phys_length
     myCaseParameters.set<PHYS_CHAR_VELOCITY>(0.1); //char_phys_vel
-    myCaseParameters.set<HAS_SURFACE_TENSION>(true);
-    myCaseParameters.set<SURFACE_TENSION_COEFFICIENT>(0.0661);
-    myCaseParameters.set<INITIAL_VELOCITY>({0.,0., -6.03});
-    myCaseParameters.set<TRANSITION_THRESHOLD>(1e-3);
-    myCaseParameters.set<LONELY_THRESHOLD>(1.0);
-  
-  
-    myCaseParameters.set<FreeSurface::DROP_ISOLATED_CELLS>(true);
-    
-   
-  
+    myCaseParameters.set<INITIAL_FALLING_SPEED>({0., 0., -6.03});
 
+    myCaseParameters.set<FreeSurface::DROP_ISOLATED_CELLS>(true);
+    myCaseParameters.set<FreeSurface::HAS_SURFACE_TENSION>(true);
+    myCaseParameters.set<FreeSurface::SURFACE_TENSION_PARAMETER>(0.0661);
+    myCaseParameters.set<FreeSurface::TRANSITION>(1e-3);
+    myCaseParameters.set<FreeSurface::LONELY_THRESHOLD>(1.0);
   }
   myCaseParameters.fromCLI(argc, argv);
 
-  
   /// === Step 3: Create Mesh ===
   Mesh mesh = createMesh(myCaseParameters);
 
@@ -490,33 +461,8 @@ int main(int argc, char** argv)
 
   /// === Step 7: Definition of Initial, Boundary Values, and Fields ===
   setInitialValues(myCase);
-  
-
 
   /// === Step 8: Simulate ===
   simulate(myCase); 
 
-   
-    //T force_factor = T(1) / converter.getConversionFactorForce() * converter.getConversionFactorMass();
-
-    // Convert kg / s^2
-    // Basically it is multiplied with s^2 / kg = s^2 * m^3 / (kg * m^2 * m) = 1. / (velocity_factor^2 * density * length_factor)
-    //T surface_tension_coefficient_factor = std::pow(converter.getConversionFactorTime(), 2) / (density * std::pow(converter.getPhysDeltaX(), 3));
-
-    //clout << "Surface: " << surface_tension_coefficient_factor * surface_tension_coefficient << std::endl;
-    //clout << "Lattice Size: " << converter.getPhysDeltaX() << std::endl;
-
-    //FreeSurface3DSetup<T, DESCRIPTOR> free_surface_setup{ sLattice };
-
-    //free_surface_setup.addPostProcessor();
-
-    // Set variables from freeSurfaceHelpers.h
-    //sLattice.setParameter<FreeSurface::DROP_ISOLATED_CELLS>(true);
-    //sLattice.setParameter<FreeSurface::TRANSITION>(transitionThreshold);
-    //sLattice.setParameter<FreeSurface::LONELY_THRESHOLD>(lonelyThreshold);
-    //sLattice.setParameter<FreeSurface::HAS_SURFACE_TENSION>(has_surface_tension);
-    //sLattice.setParameter<FreeSurface::SURFACE_TENSION_PARAMETER>(surface_tension_coefficient_factor * surface_tension_coefficient);
-    //sLattice.setParameter<FreeSurface::FORCE_DENSITY>({ gravity_force[0] * force_factor, gravity_force[1] * force_factor, gravity_force[2] * force_factor });
-
-    
 }
