50830eee27f20f66edf27e0d16f12f7addf25b4a
Maximilian Schecher <maximilianschecher@gmail.com>
2025-11-24 15:01:57 +0100

    Completed this example regarding dynamics and fields calls in a manner equivalent to the already merged 'public-fallingDrop2d'.

--- examples/freeSurface/deepFallingDrop2d/deepFallingDrop.cpp
+++ examples/freeSurface/deepFallingDrop2d/deepFallingDrop.cpp
@@ -194,19 +194,8 @@ void prepareLattice(MyCase& myCase) {
 
   auto& converter = lattice.getUnitConverter();
 
-  /* SETTER Refactoring: Change defineDynamics to setter, remove defineDynamics call for NoDynamics, as it is the default now
-  // Material=0 -->do nothing
-  lattice.defineDynamics<NoDynamics<T,DESCRIPTOR>>(geometry, 0);
-  // Material=1 --> dynamics
-  lattice.defineDynamics<SmagorinskyForcedBGKdynamics<T,DESCRIPTOR>>(geometry, 1);
-  */
   dynamics::set<SmagorinskyForcedBGKdynamics>(lattice, geometry.getMaterialIndicator({1}));
 
-  /* SETTER Refactoring: Change defineDynamics to boundary-setter
-  // Material=2 -->no-slip boundary
-  lattice.defineDynamics<BounceBack<T,DESCRIPTOR>>(geometry, 2);
-  //setSlipBoundary<T,DESCRIPTOR>(lattice, geometry, 2);
-  */
   boundary::set<boundary::BounceBack>(lattice, geometry, 2);
   //setSlipBoundary<T,DESCRIPTOR>(lattice, geometry, 2);
 
@@ -216,47 +205,21 @@ void prepareLattice(MyCase& myCase) {
   // prepareFallingDrop(...);
   T lattice_size = char_phys_length / resolution;
 
-  /* SETTER Refactoring: These variables are no longer used
-  AnalyticalConst2D<T,T> zero( 0. );
-  AnalyticalConst2D<T,T> one( 1. );
-  AnalyticalConst2D<T,T> two( 2. );
-  AnalyticalConst2D<T,T> four( 4. );
-  */
   FreeSurfaceDeepFallingDrop2D<T,DESCRIPTOR> cells_analytical{ lattice_size, {0., 1., 2.}};
   FreeSurfaceDeepFallingDrop2D<T,DESCRIPTOR> mass_analytical{ lattice_size, {0., 0.5, 1.}};
 
   AnalyticalConst2D<T,T> force_zero{0., 0.};
 
-  /* SETTER Refactoring: Change defineField to setter
-  for (int i: {0,1,2}) {
-    lattice.defineField<FreeSurface::MASS>(geometry, i, zero);
-    lattice.defineField<FreeSurface::EPSILON>(geometry, i, zero);
-    lattice.defineField<FreeSurface::CELL_TYPE>(geometry, i, zero);
-    lattice.defineField<FreeSurface::CELL_FLAGS>(geometry, i, zero);
-    lattice.defineField<descriptors::FORCE>(geometry, i, force_zero);
-  }*/
   fields::set<FreeSurface::MASS>(lattice, geometry.getMaterialIndicator({0,1,2}), 0.);
   fields::set<FreeSurface::EPSILON>(lattice, geometry.getMaterialIndicator({0,1,2}), 0.);
   fields::set<FreeSurface::CELL_TYPE>(lattice, geometry.getMaterialIndicator({0,1,2}), 0.);
   fields::set<FreeSurface::CELL_FLAGS>(lattice, geometry.getMaterialIndicator({0,1,2}), 0.);
   fields::set<descriptors::FORCE>(lattice, geometry.getMaterialIndicator({0,1,2}), force_zero);
 
-  /* SETTER Refactoring: Change defineField to setter
-  lattice.defineField<FreeSurface::CELL_TYPE>(geometry, 1, cells_analytical);
-  lattice.defineField<FreeSurface::MASS>(geometry, 1, mass_analytical);
-  lattice.defineField<FreeSurface::EPSILON>(geometry, 1, mass_analytical);
-  */
   fields::set<FreeSurface::CELL_TYPE>(lattice, geometry.getMaterialIndicator({1}), cells_analytical);
   fields::set<FreeSurface::MASS>(lattice, geometry.getMaterialIndicator({1}), mass_analytical);
   fields::set<FreeSurface::EPSILON>(lattice, geometry.getMaterialIndicator({1}), mass_analytical);
 
-  /* SETTER Refactoring: Change defineField to setter
-  for (int i: {0,2}) {
-    //lattice.defineField<FreeSurface::MASS>(geometry, i, one);
-    lattice.defineField<FreeSurface::EPSILON>(geometry, i, one);
-    lattice.defineField<FreeSurface::CELL_TYPE>(geometry, i, four);
-  }
-  */
   fields::set<FreeSurface::MASS>(lattice, geometry.getMaterialIndicator({0,2}), 1.);
   fields::set<FreeSurface::EPSILON>(lattice, geometry.getMaterialIndicator({0,2}), 1.);
   fields::set<FreeSurface::CELL_TYPE>(lattice, geometry.getMaterialIndicator({0,2}), 4.);
@@ -267,9 +230,7 @@ void prepareLattice(MyCase& myCase) {
 
   T force_factor = T(1) / converter.getConversionFactorForce() * converter.getConversionFactorMass();
   AnalyticalConst2D<T,T> force_a{gravity[0] * force_factor, gravity[1] * force_factor};
-  /* SETTER Refactoring: Change defineField to setter
-  lattice.defineField<descriptors::FORCE>(geometry.getMaterialIndicator({1}), force_a);
-  */
+
   fields::set<descriptors::FORCE>(lattice, geometry.getMaterialIndicator({1}), force_a);
 
   // Convert kg / s^2
